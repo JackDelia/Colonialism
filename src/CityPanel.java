772eee122651b7be7a37469b0ce3c0da06d8ebdf
JackDelia <captjack17@gmail.com>
2016-06-01 16:15:46 -0700

    create CityPanel class

--- src/CityPanel.java
+++ src/CityPanel.java
@@ -1,6 +1,19 @@
+import java.awt.LayoutManager;
+
 import javax.swing.JPanel;
 
 
 public class CityPanel extends JPanel {
 
+	private City city;
+
+	public CityPanel(City city) {
+		super();
+		this.city = city;
+	}
+	
+	
+
+	
+	
 }

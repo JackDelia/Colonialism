865fbe2980f0a99bf89ae608850d040527f44ca5
JackDelia <captjack17@gmail.com>
2016-07-07 17:18:17 -0700

    BGM

--- src/GameRunner.java
+++ src/GameRunner.java
@@ -1,8 +1,23 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import sun.audio.AudioPlayer;
+import sun.audio.AudioStream;
+
 
 public class GameRunner {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		Game g = new Game();
+		InputStream in = new FileInputStream("df.wav");
+
+		// Create an AudioStream object from the input stream.
+		AudioStream as = new AudioStream(in);         
+
+		// Use the static class member "player" from class AudioPlayer to play
+		// clip.
+		AudioPlayer.player.start(as);
 		g.run();
 
 	}
